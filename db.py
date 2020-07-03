14cad3b0917a5fc3cdd36a185cc3388fae6d057b
Owen Stillman <owenstillman@live.co.uk>
2020-07-03 16:01:28 +0100

    Front-end understanding complete

--- db.py
+++ db.py
@@ -199,7 +199,7 @@ class FetchChannels:
         channels = []
         grid_row = 1
         for channel in self.cursor.fetchall():
-            channels.append([channel[0], channel[1].lower(), grid_row, channel[2]])
+            channels.append([channel[0], channel[1].lower(), grid_row, channel[2], channel[3]])
             if channel[0] % 2 == 0:
                 grid_row = grid_row + 1
         self._channels = channels
--- static/shows/js/add.js
+++ static/shows/js/add.js
@@ -7,14 +7,91 @@ let begin = {
     }
 };
 
+let submitChecks = {
+    init: function(){
+        if (this.checkShowTitle() && this.checkServiceSelection()){
+            console.info("We can begin");
+            $("div.2").hide();
+            return true
+        }
+    },
+    checkShowTitle(){
+        if($("section.add .elements h2#title").hasClass("unedited")){
+            return false;
+        }
+        else{
+            return true
+        }
+    },
+    checkServiceSelection: function(){
+        if ($("section.add .elements div#service_grid img.chosen").length > 0){
+            return true
+        }
+        else{
+            return false;
+        }
+    },
+};
+
+let submit_to_search = {
+    init: function(){
+        if (submitChecks.init()){
+            submit_to_search.proceed();
+        }
+    },
+    proceed: function(){
+        let service = $("section.add .elements div#service_grid img.chosen").attr("alt");
+        let title = $("section.add .elements h2#title").text();
+        console.info(`Service: ${service} / Title: ${title}`);
+    },
+};
+
 let bindings = {
     init: function(){
         this.clickLiveTV();
+        this.clickSubmit();
+        this.showTitleBeginEntry();
+        this.serviceSelection();
     },
     clickLiveTV:function(){
         $("section.add div.1 #live_tv").click(function(){
             $("div.1").hide();
             $("div.2").show();
+        });
+    },
+    clickSubmit: function(){
+        $("section.add div.2 button").click(function(){
+            submit_to_search.init();
+        });
+    },
+    showTitleBeginEntry: function(){
+        $("section.add .elements h2#title").click(function(){
+            if ($(this).hasClass("unedited")){
+                $(this).text("").removeClass("unedited");
+            }
+        });
+    },
+    serviceSelection: function(){
+        $("section.add .elements div#service_grid img").click(function(){
+            $("section.add .elements div#service_grid img").removeClass("chosen").css("filter", "opacity(20%)");
+            $(this).css("filter", "opacity(100%)").addClass("chosen")
+        });
+    },
+};
+
+let ajaxCalls = {
+    ajaxCallData: function (method, url, data) {
+        return $.ajax({
+            method: method,
+            url: url,
+            data: JSON.stringify(data),
+            dataType: 'json',
+        });
+    },
+    ajaxCall: function (method, url) {
+        return $.ajax({
+            method: method,
+            url: url,
         });
     }
 };
