3826edd3f7211e22dc5b4606f62dd8cb7a0ff0cc
Owen Stillman <owenstillman@live.co.uk>
2020-07-03 17:31:58 +0100

    Simple Searching Implemented

--- tests/addShow.test.py
+++ tests/addShow.test.py
@@ -1,16 +1,34 @@
 from os import path
 import sys
 import unittest
+from unittest import mock
+import json
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 from Shows import addShows
 
 class testAddShows(unittest.TestCase):
 
     def setUp(self):
-        self.AddShow = addShows.AddShow()
-        self.AddShow.details = {"name": "show1", "channel": "bbc1"}
+        self.details = {"name": "Masterchef", "service": "512"}
+        self.AddShow = addShows.AddShow(512)
+
+    def test_finds_show(self):
+        show_times = self.AddShow.search("Masterchef")
+        assert show_times[0] == ['Friday July 03, 2020 19:30:00', 13116]
+
+    def test_show_not_found(self):
+        show_times = self.AddShow.search("Notathing")
+        assert show_times[0] == ["Error, show not found", 0]
     
-    def test_takes_show(self):
+    def test_show_can_be_found(self):
+        show_times = self.AddShow.search("Different Show")
+        assert show_times[0] == ["Error, show not found", 0]
+        self.AddShow.offset = self.AddShow.offset + 1
+        show_times = self.AddShow.search("Different Show")
+        assert show_times[0] == ['Friday July 03, 2020 16:30:00', 13474]
+
+    
+
         
 
 if __name__ == '__main__':
