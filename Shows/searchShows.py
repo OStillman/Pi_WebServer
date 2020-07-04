062c2ed8aec754be2df0483398f77060d5c6e136
Owen Stillman <owenstillman@live.co.uk>
2020-07-04 15:06:00 +0100

    Finish Show Search

---
+++ Shows/searchShows.py
@@ -0,0 +1,65 @@
+import urllib.request as request
+import urllib.parse as parse
+import json
+import time
+
+from tests import test_show_data as test_data
+
+class SearchShow():
+    def __init__(self, service, offset=0):
+        self.url = "https://www.freesat.co.uk/tv-guide/api/{}/?channel={}".format(offset, service)
+        self.offset = offset
+
+    @property
+    def offset(self):
+        return self._offset
+
+    @offset.setter
+    def offset(self, offset):
+        self._offset = offset
+
+    def fetch(self, test=False):
+        if not test:
+            req = request.Request(url=self.url, method='GET')
+            request.urlopen(req)
+            print(request.urlopen(req).read().decode('utf-8'))
+            self.show_data = json.loads(request.urlopen(req).read().decode('utf-8'))
+        else:
+            if self.offset == 0:
+                self.show_data = test_data.data1
+            else:
+                self.show_data = test_data.data2
+
+    def check_offset(self):
+        if (self.offset > 7):
+            return False
+        else:
+            return True
+        
+    def search(self, supplied_name):
+        listing = []
+
+        if (self.check_offset()):
+            self.fetch(test=True)
+            for show in self.show_data[0]['event']:
+                if self.checkMatch(show["name"], supplied_name):
+                    listing.append(self.pullOutInfo(show))
+            if len(listing) == 0:
+                listing.append(["Error, show not found", self.offset])
+        else:
+            listing.append(["Error, further than 7 days", self.offset])
+        return listing
+
+    def checkMatch(self, show_name, supplied_name):
+        if supplied_name.lower() in show_name.lower():
+            return True
+        else:
+            return False
+
+    def pullOutInfo(self, matched_show):
+        return [self.fixDateTime(matched_show["startTime"]), matched_show['evtId']]
+
+
+    def fixDateTime(self, epoch):
+        return time.strftime('%A %B %d, %Y %H:%M:%S', time.localtime(epoch))
+
--- tests/addShow.test.py
+++ tests/addShow.test.py
@@ -3,29 +3,36 @@ import sys
 import unittest
 from unittest import mock
 import json
-sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-from Shows import addShows
 
-class testAddShows(unittest.TestCase):
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from Shows import searchShows
+
+class testSearchShow(unittest.TestCase):
 
     def setUp(self):
         self.details = {"name": "Masterchef", "service": "512"}
-        self.AddShow = addShows.AddShow(512)
+        self.SearchShow = searchShows.SearchShow(512)
 
     def test_finds_show(self):
-        show_times = self.AddShow.search("Masterchef")
+        show_times = self.SearchShow.search("Masterchef")
         assert show_times[0] == ['Friday July 03, 2020 19:30:00', 13116]
 
     def test_show_not_found(self):
-        show_times = self.AddShow.search("Notathing")
+        show_times = self.SearchShow.search("Notathing")
         assert show_times[0] == ["Error, show not found", 0]
     
     def test_show_can_be_found(self):
-        show_times = self.AddShow.search("Different Show")
+        show_times = self.SearchShow.search("Different Show")
         assert show_times[0] == ["Error, show not found", 0]
-        self.AddShow.offset = self.AddShow.offset + 1
-        show_times = self.AddShow.search("Different Show")
+        self.SearchShow.offset = self.SearchShow.offset + 1
+        show_times = self.SearchShow.search("Different Show")
         assert show_times[0] == ['Friday July 03, 2020 16:30:00', 13474]
+
+    def test_no_over_7(self):
+        self.SearchShow.offset = 8
+        show_times = self.SearchShow.search("Different Show")
+        assert show_times[0] == ['Error, further than 7 days', 8]
+
 
     
 
