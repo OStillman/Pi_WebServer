d15e4b37a8eb98deaa9ff6eccf24b3c1c25a7dcd
Owen Stillman <owenstillman@live.co.uk>
2020-07-25 14:10:17 +0100

    Bug Squash for Ep/S finding issues

--- Shows/searchDetails.py
+++ Shows/searchDetails.py
@@ -71,7 +71,7 @@ class SearchShowDetail():
             return data
 
     def attemptSeriesEpFetch(self, description):
-        split_description = description.split("(")[-1].split(")")[0]
+        '''split_description = description.split("(")[-1].split(")")[0]
         episode_fetch = split_description.split("Ep")[1]
         series_fetch = split_description.split("Ep")[0].split("S")[1].split(" ")[0]
         print(len(series_fetch))
@@ -79,7 +79,95 @@ class SearchShowDetail():
         if len(series_fetch) == 1 and len(episode_fetch) == 1:
             return [True, series_fetch, episode_fetch]
         else:
+            return [False]'''
+
+        ep_num = self.fetch_ep(description)
+        print(ep_num)
+
+        s_num = self.fetchSeries(description)
+        print(s_num)
+
+        if ep_num.isnumeric():
+            print("EP is numeric")
+            if s_num.isnumeric():
+                # We can get away with not having a series, some shows only list their episode numbers
+                print("Series numeric")
+                return [True, int(s_num), int(ep_num)]
+            else:
+                print("No series, assuming S0")
+                return [True, 0, int(ep_num)]
+        else:
             return [False]
+
+
+        
+
+    def fetch_ep(self, description):
+        '''
+        Fetch the episode number from the description
+        '''
+        # Find the Char position of "Ep"
+        find_point = description.find("Ep")
+        print("=== Ep & Show search")
+        print(find_point)
+        # It'll give us the "E" part, so we need to then plus 2 to get the number
+        num_point = find_point + 2
+        is_numeric = True
+        final_num = ""
+        # -1 would indicate we couldn't find Ep in the description
+        if find_point == -1:
+            return False
+        else:
+            while is_numeric:
+                # Check this is numeric
+                if description[num_point].isnumeric():
+                    # If so, we can add it to our description
+                    final_num+= description[num_point]
+                else:
+                    # if it's not, we can stop the loop - we have our ep number
+                    is_numeric = False
+                # increase mnum_point for the next loop to look at the next char
+                num_point+=1
+            return final_num
+
+    def fetchSeries(self, description, start=0):
+        '''
+        Fetch series, similar to fetch ep but has a few more complexities...
+        @Param start = 0, can be used to set the "search from char" position for S's in the string elsewhere
+        '''
+        # Find the S, using the start char position
+        find_series_point = description.find("S", start)
+        print(find_series_point)
+        # Only add 1 this time, S is only 1 char long
+        num_point = find_series_point + 1
+        is_numeric = True
+        initially_numeric = False
+        final_num = ""
+        # Minus 1 indicates there's no sign of a S remaining i.e. series isn't in the desc
+        if find_series_point == -1:
+            return False
+        else:
+            while is_numeric:
+                if description[num_point].isnumeric():
+                    print("IS numeric")
+                    print(description[num_point])
+                    # Add this number
+                    final_num = final_num + description[num_point]
+                    # This does have numbers, so initally numeric allows us to return these numbers at the end
+                    initially_numeric = True
+                    num_point+=1
+                else:
+                    # Not numeric, we've (maybe) found the series
+                    is_numeric = False
+                print(final_num)
+            # If it was initially numeric, we have a number, so this will be the series
+            if initially_numeric:
+                print(final_num)
+                return final_num
+            else:
+                # We haven't found a number, it was initially numeric, re-call the function using the current char as the start search location
+                return(self.fetchSeries(description, start=num_point))
+                
 
         
 
