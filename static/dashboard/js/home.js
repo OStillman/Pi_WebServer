7a41cb077b553aa7b9c8c02da871de0d76a00611
Owen Stillman <owenstillman@live.co.uk>
2020-08-24 11:11:15 +0100

    Front-page template complete

---
+++ static/dashboard/js/home.js
@@ -0,0 +1,59 @@
+var header = new Vue({
+    el: ".header",
+    data: {
+        datetime: "",
+        notices: "No Notices"
+    },
+    methods: {
+        getCurrentTime: function(){
+            this.datetime = currentTime();
+        }
+    }
+});
+
+var presence_detection = new Vue({
+    el: ".presence-detection",
+    data: {
+        owen_status: "in",
+        kay_status: "out"
+    }
+});
+
+var food_tonight = new Vue({
+    el: ".info-pane.r",
+    data: {
+        today: "Spag Bowl",
+        tomorrow: "Bangers and Mash"
+    }
+});
+
+var on_tonight = new Vue({
+    el: ".info-pane.l",
+    data: {
+        seven: "-",
+        eight: "Gogglebox",
+        nine: "-",
+        ten: "Celebrity Masterchef"
+    }
+});
+
+function currentTime(){
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    var date = d.getDate();
+    var month = monthNames[d.getMonth()];
+    var y = d.getFullYear();
+    if (m.toString().length == 1){
+        m = `0${m}`;
+    }
+    return `${h}:${m} ${date} ${month} ${y}`;
+}
+
+setInterval(function(){
+    header.getCurrentTime();
+}, 1000)
