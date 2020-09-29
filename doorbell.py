cad27b81c95f3cec31cdabc43b2f81511efdc71c
Owen Stillman <owenstillman@live.co.uk>
2020-09-29 12:54:54 +0100

    Doorbell fix

--- doorbell.py
+++ doorbell.py
@@ -1,5 +1,6 @@
 import os
 import notify
+import paho.mqtt.client as mqtt
 
 class Doorbell():
     def __init__(self, data):
@@ -8,7 +9,12 @@ class Doorbell():
         self.notify()
 
     def ringBell(self):
-        os.system("aplay -D bluealsa /home/pi/Documents/Dev/Dingdong.wav")
+        #os.system("aplay -D bluealsa /home/pi/Documents/Dev/Dingdong.wav")
+        broker_address="192.168.68.116"
+        client = mqtt.Client("doorbell") #create new instance
+        client.connect(broker_address) #connect to broker
+        client.publish("doorbell/back","Pressed")#publish
+
 
     def notify(self):
         notify.Notify(title="DING DONG", message="{} is ringing!".format(self.doorbell))
